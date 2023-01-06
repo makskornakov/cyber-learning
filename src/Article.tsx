950db8eac59864b495a9c8c452b208fccaa6c328
Maksim Kornakov <makskornakov@ya.ru>
2023-01-07 00:42:52 +0300

    Update Article.tsx

--- src/Article.tsx
+++ src/Article.tsx
@@ -1,4 +1,4 @@
-import { useLocation, Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import { ArticleContainer, FooterContainer, LinkHome } from './Article.styled';
 import { HomeContainer } from './Home.styled';
 import infoMap from './infoMap.json';
